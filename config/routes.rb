93564c279c248f5522bba2216a2edc5ddfa4b372
Aarii08 <140925809+Aarii08@users.noreply.github.com>
2023-08-02 08:08:59 +0000

    [Finish] 3-1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'lists/new'
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/edit'
+  # urlの/homes/という部分が不要なのでルーティングを少し書き換える。
+  get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
