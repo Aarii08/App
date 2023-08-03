54b055fe8360ee5adf9137811fce294ca783981c
Aarii08 <140925809+Aarii08@users.noreply.github.com>
2023-08-03 04:06:19 +0000

    [Finish]3-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'lists/new'
+  get 'lists/edit'
+  get '/top' => 'homes#top'
   post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
-  get 'lists/show'
-  get 'lists/edit'
-  # urlの/homes/という部分が不要なのでルーティングを少し書き換える。
-  get '/top' => 'homes#top'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # .../lists/1や.../lists/3に該当する
+  get 'lists/:id' => 'lists#show', as: 'list'
+
 end
